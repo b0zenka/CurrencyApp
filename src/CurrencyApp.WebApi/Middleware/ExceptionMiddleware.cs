e66ed2d4fff2e8220b66a5305f8cd915632ce292
b0zenamazur <mazur.bozena94@gmail.com>
2025-11-07 16:52:47 +0100

    Add middleware

---
+++ src/CurrencyApp.WebApi/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+using Serilog;
+
+namespace CurrencyApp.WebApi.Middleware;
+public sealed class ExceptionMiddleware : IMiddleware
+{
+    public async Task InvokeAsync(HttpContext ctx, RequestDelegate next)
+    {
+        try
+        {
+            await next(ctx);
+        }
+        catch (OperationCanceledException)
+        {
+            ctx.Response.StatusCode = (int)HttpStatusCode.RequestTimeout;
+        }
+        catch (Exception ex)
+        {
+            Log.Error(ex, "Unhandled");
+            ctx.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            await ctx.Response.WriteAsJsonAsync(new { error = "internal_error" });
+        }
+    }
+}
